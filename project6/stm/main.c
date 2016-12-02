f8fd88fcc4e89a25b00830e97e23960d126cf821
James Moretti <jim5776@rit.edu>
2016-12-02 18:17:43 -0500

    Got PWM working again

--- project6/stm/main.c
+++ project6/stm/main.c
@@ -2,9 +2,10 @@
 #include "GPIO.h"
 #include "Timer.h"
 
-#define PWM_PERIOD  (200) // 20ms
-#define MIN_VOLTAGE (-5)
-#define MAX_VOLTAGE (5)
+#define PWM_PRESCALAR (200)
+#define PWM_PERIOD    (400) // 20ms
+#define MIN_VOLTAGE   (-5)
+#define MAX_VOLTAGE   (5)
 
 void PWM_Init(void);
 int8_t readVoltage(void);
@@ -19,6 +20,7 @@ int main(void) {
 
 	// PWM and GPIO setup
 	PWM_Init();
+	/*
 	GPIOA_Init();
 	GPIO_Resistor(GPIOA, PIN_0, GPIO_RES_PULLDOWN);
 	GPIO_Resistor(GPIOA, PIN_1, GPIO_RES_PULLDOWN);
@@ -28,9 +30,11 @@ int main(void) {
 	GPIO_Mode(GPIOA, PIN_1, GPIO_MODE_INPUT);
 	GPIO_Mode(GPIOA, PIN_2, GPIO_MODE_INPUT);
 	GPIO_Mode(GPIOA, PIN_3, GPIO_MODE_INPUT);
-
+*/
+	
 	// TODO wait until button is pressed
-
+	while (1);
+	
 	// Display voltage using servo
 	while (1) {
 		int8_t voltage = readVoltage();
@@ -67,33 +71,31 @@ int8_t readVoltage() {
 
 /* Servo PWM Initialize */
 void PWM_Init() {
-	// Enable TIM2 clock
-	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
+	
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN; // Enable TIM2 clock
 
-	/* Started at 8000 and modified until I got 20ms pulses on the
-	 * oscilloscope with TIMx->ARR set to 200 */
-	TIM2->PSC = 400;         // Set prescalar
-	TIM2->EGR |= TIM_EGR_UG; // Trigger timer event to force load prescalar
+	// TIM2 timing setup
+	TIM2->PSC = PWM_PRESCALAR;            // Set prescalar
+	TIM2->ARR = PWM_PERIOD;               // Set period TODO consider adjusting this for more resolution
 	
-	// Set TIM2 Channel 1 to PWM mode
-	TIM2->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;
-	TIM2->CCMR1 |= TIM_CCMR1_OC1PE; // Channel 1 Preload Enable
+	// TIM2 channel setup
+	TIM2->CCMR1 |= TIM_CCMR1_OC1M_1;      // Set TIM2_CH1 to output compare mode
+	TIM2->CCMR1 |= TIM_CCMR1_OC1M_2;
+	TIM2->CCMR1 |= TIM_CCMR1_OC1PE;       // Set TIM2_CH1 output compare Preload Enable
+	TIM2->CR1 |= TIM_CR1_ARPE;            // Enable auto-reload of preload
+	TIM2->CCER |= TIM_CCER_CC1E;          // Enable channel output bit for TIM2_CH1
 	
-	// Enable auto-reload of preload
-	TIM2->CR1 |= TIM_CR1_ARPE;
-	
-	// Enable channel output bit for TIM2
-	TIM2->CCER |= TIM_CCER_CC1E;
-	
-	// Trigger update to load the above settings
-	TIM2->EGR |= TIM_EGR_UG;
-	
-	// Set period of PWM
-	TIM2->ARR = PWM_PERIOD; // TODO consider adjusting this for more resolution
-	TIM2->EGR |= TIM_EGR_UG;
+	// Finalize
+	TIM2->EGR |= TIM_EGR_UG;              // Trigger update to load the above settings into TIM2
+
+	// GPIO Alternate Function mode for PWM output
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;  // Enable GPIOA
+	GPIOA->MODER &= ~GPIO_AFRL_AFRL0;     // Clear MODER
+	GPIOA->MODER |= GPIO_MODER_MODER0_1;  // Set GPIOA to Alternate Function mode
+	GPIOA->AFR[0] |= 0x01;                // TIM2_CH1 output on GPIO pin PA0
 
 	// Start PWM
-	TIM2->CR1 |= TIM_CR1_CEN;
+	TIM2->CR1 |= TIM_CR1_CEN;             // TIM2 Counter enable
 }
 
 void setServoPosition(uint8_t position) {
